06ca5bd0b0d287e2ff0b9569dc82473d29270579
he.fei(何飞) <he.fei@advantech.com.cn>
2020-03-10 10:12:48 +0800

    delete postgres.json add getpostgresqlsecretinfo.py

---
+++ getpostgresqlsecretinfo.py
@@ -0,0 +1,29 @@
+import base64, json, sys
+def parseConfig(secretinfofile,postgresinfofile):
+    map = {}
+    with open(secretinfofile,'r',encoding='UTF-8') as f:
+        secretinfo = json.load(f)
+        data = secretinfo['data']['ENSAAS_SERVICES']
+        dbinfo = base64.b64decode(data).decode("ascii")
+        dbinfo = json.loads(dbinfo)
+        postgresqlinfo=dbinfo['postgresql'][0]
+        map['database'] = postgresqlinfo['credentials']['database']
+        map['host'] = postgresqlinfo['credentials']['host']
+        map['password'] = postgresqlinfo['credentials']['password']
+        map['port'] = postgresqlinfo['credentials']['port']
+        map['uri'] = postgresqlinfo['credentials']['uri']
+        map['username'] = postgresqlinfo['credentials']['username']
+    with open(postgresinfofile,'r',encoding='UTF-8') as f:
+        postgresinfo = json.load(f)
+        postgresinfo['database'] = map['database']
+        postgresinfo['user'] = map['username']
+        postgresinfo['password'] = map['password']
+        postgresinfo.secureJsonData['password'] = map['password']
+        postgresinfo['url'] = map['host']
+        print postgresinfo
+        f = open(postgresinfofile, 'w')
+        f.write(json.dumps(postgresinfo, indent=2))
+        f.close()
+
+if __name__ == '__main__':
+    parseConfig(sys.argv[1], sys.argv[2])
